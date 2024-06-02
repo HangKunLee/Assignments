02f3cd4eeb485475d0641f100e3fbcfa425e1364
kun <1541743082@qq.com>
2024-06-02 21:11:49 +0930

    q13

--- question13.sh
+++ question13.sh
@@ -1,5 +1,5 @@
 git checkout -b assignment-branch
-rm *.sh
+rm question03.sh question04.sh question05.sh question07.sh question08.sh question09.sh question10.sh question11.sh question12.sh
 git add .
 touch file13.txt
 git add .
