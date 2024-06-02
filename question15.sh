4ca22fe88ed2f437169629d44dbdfeb82a63e142
kun <1541743082@qq.com>
2024-06-02 20:44:53 +0930

    q15

---
+++ question15.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ian-knight-uofa/git-practice-01.git
+cd git-practice-01
+git fetch origin
+git checkout main
+git merge origin/branch1
+git add .
+git commit -m "Resolved merge conflicts between main and branch1"
