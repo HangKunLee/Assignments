ad74b8bee12dbf5c61fb121a203a9beb0a32dcd6
kun <1541743082@qq.com>
2024-06-03 17:24:28 +0930

    Remove old .sh files and add file13.txt

--- question04.sh
+++
@@ -1,3 +0,0 @@
-touch file3.txt
-git add file3.txt
-git commit -m "Add file3.txt"
--- question13.sh
+++ question13.sh
@@ -1,7 +1,8 @@
-git checkout -b assignment-branch
+git checkout -b question13-branch
 rm question03.sh question04.sh question05.sh question07.sh question08.sh question09.sh question10.sh question11.sh question12.sh
 git add .
 touch file13.txt
 git add file13.txt
 git commit -m "Remove old .sh files and add file13.txt"
+git push origin question13-branch
 
